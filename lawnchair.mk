2b189d396dc8c0b0f2148ce1531070c08bc74eb1
Juhyung Park <qkrwngud825@gmail.com>
2022-02-12 18:59:36 +0900

    Update to Lawnchair 12 Alpha 5
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- lawnchair.mk
+++ lawnchair.mk
@@ -1,8 +1,8 @@
 include $(call first-makefiles-under,$(LOCAL_PATH))
 
 PRODUCT_COPY_FILES += \
-    vendor/lawnchair/etc/permissions/privapp-permissions-ch.deletescape.lawnchair.ci.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-ch.deletescape.lawnchair.ci.xml \
-    vendor/lawnchair/etc/sysconfig/lawnchair-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-whitelist.xml
+    vendor/lawnchair/etc/permissions/privapp-permissions-app.lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-app.lawnchair.xml \
+    vendor/lawnchair/etc/sysconfig/app.lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/app.lawnchair-hiddenapi-package-whitelist.xml
 
 PRODUCT_PACKAGES += \
     QuickstepSwitcherOverlay \
