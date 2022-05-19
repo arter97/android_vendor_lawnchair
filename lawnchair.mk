5fe8e0ab3488d405a7ee1c0c214002ba0bf54354
Juhyung Park <qkrwngud825@gmail.com>
2022-05-20 04:07:12 +0900

    Update to Lawnchair 12.1 Alpha 3
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- lawnchair.mk
+++ lawnchair.mk
@@ -2,7 +2,8 @@ include $(call first-makefiles-under,$(LOCAL_PATH))
 
 PRODUCT_COPY_FILES += \
     vendor/lawnchair/etc/permissions/privapp-permissions-app.lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-app.lawnchair.xml \
-    vendor/lawnchair/etc/sysconfig/app.lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/app.lawnchair-hiddenapi-package-whitelist.xml
+    vendor/lawnchair/etc/sysconfig/app.lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/app.lawnchair-hiddenapi-package-whitelist.xml \
+    vendor/lawnchair/priv-app/Lawnchair/lib/arm64/libnrb.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/Lawnchair/lib/arm64/libnrb.so
 
 PRODUCT_PACKAGES += \
     QuickstepSwitcherOverlay \
